15b44b2a8512b559db7ad71386aa02497f37e725
buttercup36795937 <70871834+buttercup36795937@users.noreply.github.com>
2020-09-26 19:43:34 +0800

    Add files via upload

--- 90006.py
+++ 90006.py
@@ -1,21 +1,27 @@
-string= 'I love you'
-a= string.split(' ')
-print(a)
+score=['小徐',5,9,6,8,7,10,6]
+print(max(score[1:]))
+print(min(score[1:]))
+c=sorted(score[1:],reverse=True)
+d=c[0:3]
+print(d)
+e=c[4:8]
+print(e)
+a=sum(score[1:])
+b=len(score[1:])
+print(int(a/b))
+####################################
+for x in range(1,10):
+    for i in range(1,10):
+        print('%ix%i=%2i' % (x, i, x*i), end=' ')
+    print()
+####################################
 
-################################
-
-b= string[::-1]
-s= b.split(' ')
-print(s)
-
-################################
-n="I Love you so much"
-c="so "*100
-print(n.replace("so",c))
-
-################################
 import random
 d=random.randint(1,100)
-m="I Love you so much"
-f= str("so "*d)
-print(m.replace("so",f))
+s=str(" So"*d)
+a="I love so much"
+for i in a :
+    if  i=="s":
+         c =a.replace("so",s)
+"".join(c.split())
+print(c)
