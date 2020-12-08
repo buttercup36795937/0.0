dcf65f07db938d49d41310593b621dc0c5082390
buttercup36795937 <70871834+buttercup36795937@users.noreply.github.com>
2020-12-08 15:07:47 +0800

    Add files via upload

--- 90006.py
+++ 90006.py
@@ -4,8 +4,6 @@ import requests
 import pandas as pf
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
-import time
-import datetime
 import calendar
 import time,datetime
 
@@ -13,7 +11,7 @@ now_date = time.strftime("%Y%m%d")
 day=int(now_date[-2:])
 
 
-n_days1=5
+n_days1=5#五天前
 for i in range(0,n_days1):
     
 
@@ -23,68 +21,47 @@ for i in range(0,n_days1):
     now1=now.strftime('%Y%m%d')
     
     n_days1=n_days.strftime('%Y%m%d')
-    c=str(n_days1[-4:-2])
+    c=str(n_days1[-4:-2])#幾月
     d=int(c)
-    a=str(n_days1[-2:])
+    a=str(n_days1[-2:])#幾號
     b=int(a)
-url ="https://www.ettoday.net/news/news-list.htm"
 
-key_word='0.0'
 options = webdriver.ChromeOptions()
 options.add_argument("--ignore-certificate-errors")
 driver =webdriver.Chrome(chrome_options=options)
-x = datetime.datetime.now()
 
 
-driver.get(url)
-for i in range(6):
-
-    r = requests.get("https://www.ettoday.net/news/news-list-2020-"+str(d)+"-"+str(b)+"-0.htm")
-    r.encoding = "utf8"
-    with open('html.text', "w+",newline='', encoding="utf8") as fp:
-    # print(r.text,file=fp)                                 ##可用print，也可用write
-            fp.write(r.text)
-    
-    with open('html.text', "r", encoding="utf8") as fp2:
-            r2=fp2.read() 
-    
-    page_source = r.text
-    page_source2 = page_source.split('\n')
-    soup = BeautifulSoup(r.text, "lxml")
-    list1=[]
-    list2=[]
-    list3=[]
-    list4=[]
-    
-    tag_div1=soup.find_all('div',class_="part_list_2")
-    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-    for a in tag_div1:
-        print(a.text)
-        list2.append(a.text)
-        b=b+1
-
+# driver.get(url)
 
 page=0
 with open('90006五天新聞.csv','w+',newline='', encoding="utf-8-sig") as csvfile:   #解決多一空行 newline=''
     writer = csv.writer(csvfile)
     writer.writerow(('時間','類別','標題',['連結']))
+    for i in range(5):
 
-    for i in range(8):
+        r=driver.get("https://www.ettoday.net/news/news-list-2020-"+str(d)+"-"+str(b)+"-0.htm")
+        
         page+=1
         html = driver.page_source
         sp=BeautifulSoup(html,"html.parser")
         search_time=sp.select("div.part_list_2 > h3 > span")
         search_a=sp.select("div.part_list_2 > h3 > em")
         search_b=sp.select("div.part_list_2 > h3 > a")
-        search_c=sp.select("div.part_list_2 > h3 > a")
+        # search_c=sp.select("div.part_list_2 > h3 > a")
         
         for i in range(len(search_time)):
             
             print(search_time[i].text,end=' ')
             print(search_a[i].text,end=' ')
+            print(search_b[i].text,end=' ')
             print(search_b[i].get('href'))
-            print(search_c[i].text,end=' ')
-            writer.writerow([search_time[i].text,search_a[i].text,search_c[i].text,search_b[i].get('href')])
+            writer.writerow([search_time[i].text,search_a[i].text,search_b[i].text,search_b[i].get('href')])
+        b=b+1
+
+
+
+
+ 
     
 
 
